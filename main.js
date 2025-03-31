88a7065427977149384b76a0da778522ad525e4a
David Mensah <david.mensah@hubtel.com>
2025-03-31 23:47:34 +0000

    Added a preloader to the index page

---
+++ main.js
@@ -0,0 +1,10 @@
+$(window).on("load", function () {
+    function preloader() {
+        if ($("#smoothpreloader").length) {
+            $("#smoothpreloader").delay(2000).fadeOut("slow", function () {
+                $(this).remove();
+            });
+        }
+    }
+    preloader();
+});
