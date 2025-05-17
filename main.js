e072a1d408a99b8f359fc5e23a3397168068e165
David Mensah <david.mensah@hubtel.com>
2025-05-17 21:58:26 +0000

    Revamped the entire web pages with the new designs

--- main.js
+++ main.js
@@ -9,6 +9,19 @@ $(window).on("load", function () {
     preloader();
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
     $(".showCurrentYear").html(new Date().getFullYear());
+});
+
+// Navbar scroll effect
+document.addEventListener('DOMContentLoaded', function () {
+    const navbar = document.querySelector('.navbar');
+
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
 });
